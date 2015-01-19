32eb90f80ccd6683e0de63ed1958c3f68c033c4b
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-01-19 11:59:34 +0100

    split to columns

--- R/mangow.R
+++ R/mangow.R
@@ -1,4 +1,16 @@
 #' Convert a Gower distance problem to a Manhattan distance problem
 #'
+#' @param data The data
+#'
 #' @export
-mangow <- function() NULL
+mangow <- function(data) {
+  columns <- lapply(
+    data,
+    mangow_one)
+
+  Reduce(cbind, columns)
+}
+
+mangow_one <- function(x) UseMethod("mangow_one", x)
+
+mangow_one.default <- function(x) stop("Can't manhattanize ", class(x))
