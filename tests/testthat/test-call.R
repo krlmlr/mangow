a1c6e1d8a6cccac0d4ff748fc7824e31789fea9b
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-01-19 12:46:37 +0100

    test equivalence for the Iris data set

--- tests/testthat/test-call.R
+++ tests/testthat/test-call.R
@@ -2,6 +2,11 @@ context("Call")
 
 test_that("Calling with Iris data set", {
   expect_true(is.matrix(mangow(iris)))
+
+  iris_sub <- iris[seq.int(from = 1, to = nrow(iris), length.out = 18),]
+  expect_equal(
+    unname(as.matrix(cluster::daisy(iris_sub, metric = "gower"))),
+    unname(as.matrix(cluster::daisy(mangow(iris_sub), metric = "manhattan"))))
 })
 
 test_that("Character vector not supported", {
