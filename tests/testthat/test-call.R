61f63c7203fc12018e68add91a803aa437f2d270
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-01-19 12:33:34 +0100

    test error with character

--- tests/testthat/test-call.R
+++ tests/testthat/test-call.R
@@ -3,3 +3,7 @@ context("Call")
 test_that("Calling with Iris data set", {
   expect_true(is.matrix(mangow(iris)))
 })
+
+test_that("Character vector not supported", {
+  expect_error(mangow(data.frame(x=letters[1:3], stringsAsFactors = FALSE)), "manhattanize")
+})
