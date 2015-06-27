505f04f7d93b79cbb3301eedca9a4b3363a90391
Kirill Müller <kirill.mueller@ivt.baug.ethz.ch>
2015-06-27 22:18:29 +0200

    test equivalence with StatMatch

--- tests/testthat/test-equiv.R
+++ tests/testthat/test-equiv.R
@@ -10,12 +10,28 @@ lapply(
   lapply(sets::set_power(names(columns)), unlist)[-1],
   function(sub_columns) {
     test_that(
-      sprintf("Equivalence with column(s) of type %s",
+      sprintf("cluster: Equivalence with column(s) of type %s",
               paste0(sub_columns, collapse = ", ")),
       {
         data <- do.call(expand.grid, columns[sub_columns])
         expect_equal(
           unname(as.matrix(cluster::daisy(data, metric = "gower"))),
+          unname(as.matrix(cluster::daisy(mangow(data), metric = "manhattan"))))
+      }
+    )
+  }
+)
+
+lapply(
+  lapply(sets::set_power(names(columns)), unlist)[-1],
+  function(sub_columns) {
+    test_that(
+      sprintf("StatMatch: Equivalence with column(s) of type %s",
+              paste0(sub_columns, collapse = ", ")),
+      {
+        data <- do.call(expand.grid, columns[sub_columns])
+        expect_equal(
+          unname(as.matrix(StatMatch::gower.dist(data))),
           unname(as.matrix(cluster::daisy(mangow(data), metric = "manhattan"))))
       }
     )
